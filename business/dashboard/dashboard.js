3185a1fa5f68ccd2dac517dfe7ca6a602f3bc735
FlamingFireball64 <flamingfireball64@gmail.com>
2017-12-17 10:29:26 +0000

    Add files via upload

--- business/dashboard/dashboard.js
+++ business/dashboard/dashboard.js
@@ -1,6 +1,6 @@
 window.onunload = function () {
   if (window.opener) {
-    window.opener.document.body.innerHTML = "<span onclick='location.reload();'>Restart Dashboard</span>";
+    window.opener.document.getElementById("restartDbrd").style.display = "block";
   }
 };
 
@@ -9,11 +9,33 @@ window.onload = function () {
   document.getElementById("businessName").innerHTML = settings.businessName;
   var counter = 0;
   while (counter < settings.links.length) {
-    document.getElementById("links").innerHTML = document.getElementById("links").innerHTML + "<a href='" + settings.links[counter].url + "' target='_blank'><img style='top: " + (Math.floor(counter / 3) * 60) + "px; left: " + (((counter % 3) * 20) + 20) + "%;' src='" + settings.links[counter].icon + "' class='linkIcon' title='" + settings.links[counter].name + "' onmouseover='displayAppName(" + counter + ");' /></a>";
+    document.getElementById("links").innerHTML = document.getElementById("links").innerHTML + "<img style='top: " + (Math.floor(counter / 3) * 60) + "px; left: " + (((counter % 3) * 20) + 20) + "%;' src='" + settings.links[counter].icon + "' class='linkIcon' title='" + settings.links[counter].name + "' onmouseover='displayAppName(" + counter + ");' onclick='openLink(" + counter + ");' />";
     counter = counter + 1;
   }
 };
 
 function displayAppName(linkNumber) {
   document.getElementById("appName").innerHTML = settings.links[linkNumber].name;
+}
+
+function updateStatsField(fieldNumber, value) {
+  if (window.opener) {
+    window.opener.document.getElementById("stats").getElementsByTagName("code")[fieldNumber].innerHTML = value;
+  }
+}
+
+window.setInterval(function () {
+  updateStatsField(0, self.screenLeft);
+  updateStatsField(1, self.screenTop);
+}, 10);
+
+function addHistory(text) {
+  var historyContainer = window.opener.document.getElementById("history").getElementsByTagName("div")[0];
+  var currentDate = new Date();
+  historyContainer.innerHTML = "<p>" + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds() + " - " + text + "</p>" + historyContainer.innerHTML;
+}
+
+function openLink(linkNumber) {
+  window.open(settings.links[linkNumber].url);
+  addHistory("Link opened: " + settings.links[linkNumber].name);
 }
--- business/dashboard/startDashboard.js
+++ business/dashboard/startDashboard.js
@@ -1,7 +1,8 @@
 if (localStorage.getItem("dashboardConfig") != null) {
   var dashboard = {
     "left" : 0,
-    "dashboardWindow" : window.open('app.html', '', 'width=300, height=500'),
+    "width" : 300,
+    "height" : 500,
     "windowSlide" : function () {
       dashboard.left = dashboard.left + 10;
       dashboard.dashboardWindow.moveTo(dashboard.left, 0);
@@ -12,7 +13,13 @@ if (localStorage.getItem("dashboardConfig") != null) {
       }
     }
   };
+  dashboard.dashboardWindow = window.open('app.html', '', 'width=' + dashboard.width + ', height=' + dashboard.height);
   dashboard.windowSlide();
+  window.setInterval(function () {
+    if (dashboard.dashboardWindow) {
+      dashboard.dashboardWindow.resizeTo(dashboard.width, dashboard.height);
+    }
+  }, 10);
 } else {
   document.write("Ask your system administrator to setup Dashboard");
 }
